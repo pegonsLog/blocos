823f1ec4f5be0bc7a2ecaa9dfffd5228fd53360f
pegonsLog <pegons.log@gmail.com>
2023-01-09 17:49:04 -0300

    09/01 - trabalho

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  API: 'http://localhost:3000/'
+  production: true,
+
+  firebase: {
+    apiKey: 'AIzaSyAyrsef6nU6qW8TQWo7AgO1F2MPieSLGK4',
+    authDomain: 'crowsoft-carnaval-bhtrans.firebaseapp.com',
+    databaseURL:
+      'https://crowsoft-carnaval-bhtrans-default-rtdb.firebaseio.com',
+    projectId: 'crowsoft-carnaval-bhtrans',
+    storageBucket: 'crowsoft-carnaval-bhtrans.appspot.com',
+    messagingSenderId: '298984360113',
+    appId: '1:298984360113:web:b0459b32df39a1414b7a90',
+    measurementId: 'G-TVF9YGCXPR',
+  },
 };
 
 /*
